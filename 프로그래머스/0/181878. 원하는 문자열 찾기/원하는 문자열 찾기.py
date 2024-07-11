e58c04ff35fff2f0c735d07187bce17708f30810
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-07-11 10:26:13 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 16.37 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -0,0 +1,9 @@
+def solution(myString, pat):
+    answer = 0
+    for i in range(len(myString) - len(pat) + 1):
+        if myString[i:i + len(pat)].lower() == pat.lower():
+            answer = 1
+            break
+        else:
+            answer = 0
+    return answer
