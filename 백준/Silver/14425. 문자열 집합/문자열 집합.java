ad9698db109893d8a916cff813c6e23e8bae6f4b
DYShin1 <daeyeong0306@gmail.com>
2024-10-06 16:31:35 +0900

    [Silver IV] Title: 문자열 집합, Time: 348 ms, Memory: 30836 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -9,20 +9,33 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // N개의 문자열을 HashSet에 저장
-        Set<String> set = new HashSet<>();
+        String[] set = new String[N];
+        
         for (int i = 0; i < N; i++) {
-            set.add(br.readLine());
+            set[i] = br.readLine();  // br.readLine()으로 수정
         }
-
+        Arrays.sort(set);  // Arrays.sort()로 수정
+        
         int count = 0;
-        // M개의 문자열을 입력받아 HashSet에 존재하는지 확인
-        for (int i = 0; i < M; i++) {
-            if (set.contains(br.readLine())) {
+        
+        for(int i = 0; i < M; i++){
+            if(isExist(set, br.readLine()))  // br.readLine()으로 수정
                 count++;
-            }
         }
-
         System.out.println(count);
+    }
+    
+    // isExist 메소드를 static으로 변경
+    public static boolean isExist(String[] arr, String check){
+        int l = 0;
+        int r = arr.length - 1;
+        while(l <= r){
+            int m = (l + r) / 2;
+            int compareResult = arr[m].compareTo(check);
+            if(compareResult < 0) l = m + 1;
+            else if(compareResult > 0) r = m - 1;
+            else return true;
+        }   
+        return false;
     }
 }
