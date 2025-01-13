ae02656b6a9d8443d86f3f79b769161937bc4b6c
DYShin1 <daeyeong0306@gmail.com>
2025-01-13 11:39:58 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,20 +1,24 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String answer = "";
-        
-        for(int i = 0; i < a.length(); i++){
-            if(Character.isUpperCase(a.charAt(i))){
-                answer += Character.toLowerCase(a.charAt(i));
-            }
-            else{
-                answer += Character.toUpperCase(a.charAt(i));
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String st = br.readLine();
+        char[] ch = new char[st.length()];
+
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) >= 65 && st.charAt(i) <= 90) { // 대문자인 경우
+                ch[i] = (char) (st.charAt(i) + 32);
+            } else if (st.charAt(i) >= 97 && st.charAt(i) <= 122) { // 소문자인 경우
+                ch[i] = (char) (st.charAt(i) - 32);
+            } else { // 알파벳이 아닌 경우
+                ch[i] = st.charAt(i);
             }
         }
-        System.out.println(answer);
+
+        // 결과 출력
+        System.out.println(new String(ch));
     }
 }
-
